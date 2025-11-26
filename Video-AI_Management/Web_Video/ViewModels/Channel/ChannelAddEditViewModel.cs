c10641e4c4aaef224b3e3307724a753f1c030a81
Hà Đỗ Nhật Huy <huyhd.22th@sv.daihoclongan.edu.vn>
2025-11-26 13:37:41 +0700

    FIX XONG GIAO DIỆN INDEX CỦA CHANNEL

--- Video-AI_Management/Web_Video/ViewModels/Channel/ChannelAddEditViewModel.cs
+++ Video-AI_Management/Web_Video/ViewModels/Channel/ChannelAddEditViewModel.cs
@@ -9,12 +9,12 @@ namespace Web_Video.ViewModels.Channel
     {
         [Required]
         public Guid Id { get; set; } // Thêm Id của kênh
-        [Required]
+        //[Required]
         [Display(Name = "Channel name")]
-        [RegularExpression("^[a-zA-Z]{3,15}", ErrorMessage = "Name must be between 3 and 15 characters long and can only contain letters (A-Z, a-z)")]
+        //[RegularExpression("^[a-zA-Z]{3,15}", ErrorMessage = "Name must be between 3 and 15 characters long and can only contain letters (A-Z, a-z)")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "About field is required")]
+        //[Required(ErrorMessage = "About field is required")]
         [StringLength(200, MinimumLength = 20, ErrorMessage = "About must be at least {2}, and maximum {1} characters")]
         [Display(Name = "About your channel")]
         public string About { get; set; }
